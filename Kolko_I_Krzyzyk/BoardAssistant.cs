01eabb777ec2acdded9d74eb78e67ed8b2370858
Michał Grygorowicz <grygorowicz.michal1@gmail.com>
2021-09-18 21:09:06 +0200

    zmiany nazw parametrów na angielsko języczne

--- Kolko_I_Krzyzyk/BoardAssistant.cs
+++ Kolko_I_Krzyzyk/BoardAssistant.cs
@@ -8,25 +8,25 @@ namespace Kolko_I_Krzyzyk
 {
     static class BoardAssistant
     {
-        static public bool IsWinning(int[,] b, int znak)
+        static public bool IsWinning(int[,] b, int token)
         {
-            if (b[0, 0] == znak && b[0, 1] == znak && b[0, 2] == znak) return true;
-            if (b[1, 0] == znak && b[1, 1] == znak && b[1, 2] == znak) return true;
-            if (b[2, 0] == znak && b[2, 1] == znak && b[2, 2] == znak) return true;
+            if (b[0, 0] == token && b[0, 1] == token && b[0, 2] == token) return true;
+            if (b[1, 0] == token && b[1, 1] == token && b[1, 2] == token) return true;
+            if (b[2, 0] == token && b[2, 1] == token && b[2, 2] == token) return true;
 
-            if (b[0, 0] == znak && b[1, 0] == znak && b[2, 0] == znak) return true;
-            if (b[0, 1] == znak && b[1, 1] == znak && b[2, 1] == znak) return true;
-            if (b[0, 2] == znak && b[1, 2] == znak && b[2, 2] == znak) return true;
+            if (b[0, 0] == token && b[1, 0] == token && b[2, 0] == token) return true;
+            if (b[0, 1] == token && b[1, 1] == token && b[2, 1] == token) return true;
+            if (b[0, 2] == token && b[1, 2] == token && b[2, 2] == token) return true;
 
-            if (b[0, 0] == znak && b[1, 1] == znak && b[2, 2] == znak) return true;
-            if (b[0, 2] == znak && b[1, 1] == znak && b[2, 0] == znak) return true;
+            if (b[0, 0] == token && b[1, 1] == token && b[2, 2] == token) return true;
+            if (b[0, 2] == token && b[1, 1] == token && b[2, 0] == token) return true;
 
             return false;
         }
-        static public bool IsFull(int[,] b, int znak)
+        static public bool IsFull(int[,] b, int token)
         {
             for (int i = 0; i < b.GetLength(0); i++) for (int j = 0; j < b.GetLength(1); j++)
-                    if (b[i, j] == znak) return false;
+                    if (b[i, j] == token) return false;
             return true;
         }
     }
